2fdd914e81da905ee7e429bf7b5a6138963c07e9
Manny29-dev <imman.chinnu@gmail.com>
2023-08-03 22:29:13 +0530

    codded

--- practice.c
+++ practice.c
@@ -1,7 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h> // Include the string.h library for strcmp function
+
+void hello(const char *name)
+{
+    if (strcmp(name, "imman") == 0) // Use strcmp to compare strings
+    {
+        printf("Welcome Boss %s!\n", name);
+    }
+    else
+    {
+        printf("Hello %s!\n", name);
+        printf("Welcome\n");
+    }
+}
+
 int main()
 {
-    printf("Hello world!");
+    char user[100]; // Use a character array to store the user's input (string)
+
+    printf("Enter your name: ");
+    scanf("%s", user); // Read the user's input into the character array
+
+    hello(user); // Pass the user's name to the hello function
 
     return 0;
 }
+
